3858ae61db6a36031b729c7384a47a88861d4e1e
kmatthews123 <kmatt999@gmail.com>
2024-06-26 14:45:55 -0700

    messing with strings

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#create my list
+my_list = ["192.168.0.5", 5060, "UP"]
+
+# display the first item in the list
+print("the first item in the list (IP): " + my_list[0] )
+
+#display the second item int he list
+print("the second item in the list (port): " + str(my_list[1]))
+
+#display the third item in the list
+print("the last item in the list (state): " + my_list[2] )
+
+#challenge portion
+print("Challenge additional...")
+
+#list
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print("IP address 1: ", iplist[3])
+print("IP address 2: ", iplist[4] )
