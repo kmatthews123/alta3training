dbeff0ed29aa4c9e63aa882538b2fef54b51a6a5
nyamong5341 <nyamoc5341@stusent.faytechcc.edu>
2024-06-26 18:04:25 +0000

    challenge1 completed

---
+++ challenges/challenge1/challenge.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    #get user name
+    user_name = input("Please provide your name: ")
+    #get the day of the week
+    day_of_week = input("Please provide the day of the week: ")
+    #print requested line
+    print("Hello, ", user_name , "! Happy ", day_of_week, sep="")
+
+main()
