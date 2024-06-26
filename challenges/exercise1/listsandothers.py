91b3cc6aedf1e3c733a8669d8c518b1aa5db4642
kmatthews123 <kmatt999@gmail.com>
2024-06-26 16:59:30 -0700

    completed bonus 2

--- challenges/exercise1/listsandothers.py
+++ challenges/exercise1/listsandothers.py
@@ -6,14 +6,16 @@ import random
 #exercise for lists, input, print, and variables
 wordbank= ["indentation", "spaces"] 
 
-tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping', 'Keith']
 
 #append number 4 to wordbank
 wordbank.append(4)
 
-#ask user for input between 0 and 20 and turn it to a number
-#num = int(input("please enter a number between 0 and 20: "))
+#ask user for input between 0 and number of students and turn it to a number
+num = int(input("please enter a number between 1 and "+ str(len(tlgstudents)) + ": ")) - 1
+student_name = tlgstudents[num]
 
-student_name = random.choice(tlgstudents)
+#uncomment below if you want to chose a random student
+#student_name = random.choice(tlgstudents)
 
 print(student_name , "always wanted to use", wordbank[2], wordbank[1], "to indent" )
